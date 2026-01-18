8f8892dfbf9b921899ffe736cba1ef50695ab7dd
iqszlong <iqszlong@qq.com>
2026-01-18 19:18:27 +0800

    feat: 调整页脚

--- src/components/Footer/index.vue
+++ src/components/Footer/index.vue
@@ -1,8 +1,6 @@
 <template>
     <div class="footer flex items-center justify-center text-xs text-white/50 gap-3">
-
-        <span v-if="isEmpty(VITE_SITE_COPYRIGHT)">&copy; 2018-{{ year }} Company inc.All Rights Reserved.</span>
-        <span v-else>{{ VITE_SITE_COPYRIGHT }}</span>
+        <span v-if="!isEmpty(VITE_SITE_COPYRIGHT)">&copy; {{VITE_SITE_STARTYEAR}}-{{ year }} {{ VITE_SITE_COPYRIGHT }}</span>
         <a v-if="!isEmpty(VITE_SITE_ICP)" href="https://beian.miit.gov.cn/" target="_blank" rel="noreferrer">
             {{ VITE_SITE_ICP }}
         </a>
@@ -19,7 +17,7 @@ import icons from '@/utils/icons'
 const { dayjs, mediaQuery, isEmpty} = utils
 const cap = icons.cap
 const year = dayjs().format('YYYY')
-const { VITE_SITE_COPYRIGHT, VITE_SITE_ICP, VITE_SITE_CAP, VITE_SITE_CAP_NO } = import.meta.env
+const { VITE_SITE_STARTYEAR,VITE_SITE_COPYRIGHT, VITE_SITE_ICP, VITE_SITE_CAP, VITE_SITE_CAP_NO } = import.meta.env
 
 </script>
 
