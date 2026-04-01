9cb27f1a174e5c0b41721e75e9ef77e601e7e449
iqszlong <iqszlong@qq.com>
2026-04-01 14:41:43 +0800

    feat: 增加标题替换

--- src/router/guard/index.js
+++ src/router/guard/index.js
@@ -1,11 +1,11 @@
 import { createBaseGuard } from "./base-guard"
 // import { createPermissionGuard } from './permission-guard'
 // import { createPageLoadingGuard } from './page-loading-guard'
-// import { createPageTitleGuard } from './page-title-guard'
+import { createPageTitleGuard } from './page-title-guard'
 
 export function setupRouterGuard(router) {
   // createPageLoadingGuard(router)
   createBaseGuard(router)
   // createPermissionGuard(router)
-  // createPageTitleGuard(router)
+  createPageTitleGuard(router)
 }
